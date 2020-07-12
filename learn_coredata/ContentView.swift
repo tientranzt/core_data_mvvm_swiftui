edc6ce6d0a3246580e7cf562fa7b0ec18f6679d5
tientran <tientran.home@gmail.com>
2020-07-12 09:57:27 +0700

    Initial Commit

---
+++ learn_coredata/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  learn_coredata
+//
+//  Created by tientran on 7/12/20.
+//  Copyright © 2020 tientran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
